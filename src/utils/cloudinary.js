275ef6ddf39f9993952270e3344b39e7a69d8728
Shaheen <shn.sdq29@gmail.com>
2024-11-12 00:58:07 +0530

    user controller

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -20,8 +20,10 @@ import fs from "fs"
                 resource_type:"auto"
             })
             //file has been uploaded successfully
-            console.log("file is uploaded on cloudinary",response.url);
+            //console.log("file is uploaded on cloudinary",response.url);
+            fs.unlinkSync(localFilePath)
             return response;
+            
      }catch(error){
           fs.unlinkSync(localFilePath) //remove the locally saved temporary file as the upload operation got failed
           return null;
