dbc58877afd95dff085c4ac059f469855d8bf43c
Shaheen <shn.sdq29@gmail.com>
2024-11-15 01:18:26 +0530

    commit

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -7,6 +7,14 @@ import { ApiResponse } from "../utils/ApiResponse.js";
 const generateAccessAndRefreshTokens = async(userId)=>{
    try{
       const user = await User.findById(userId)
+      const accessToken =user.generateAccessToken()
+      const refreshToken=user.generateRefreshToken()
+
+      user.refreshToken = refreshToken
+      await user.save({validateBeforeSave: false})
+
+      return {accessToken, refreshToken}
+
    }catch(error){
       throw new ApiError(500,"Something went wrong while generating refresh and access token")
    }
@@ -105,6 +113,29 @@ const loginUser = asyncHandler( async(req,res)=> {
    if(!isPasswordValid){
       throw new ApiError(401,"Invalid user credentials")
    }
+
+   const {accessToken,refreshToken} = await generateAccessAndRefreshTokens(user._id)
+
+   const loggedInUser = await User.findById(user._id).select("-password -refreshToken")
+
+   const options = {
+      httpOnly:true,
+      secure:true
+   }
+
+   return res.status(200)
+   .cookie("accessToken",accessToken,options)
+   .cookie("refreshToken", refreshToken,options)
+   .json(
+      new ApiResponse(
+         200,
+         {
+            user:loggedInUser,accessToken,refreshToken
+         },
+         "User logged In Successfully"
+      )
+   )
+
 
 
 })
