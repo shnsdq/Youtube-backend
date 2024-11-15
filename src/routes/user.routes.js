157650d3bd54cdbc0abbbf59c0b980aa7a2aa4ca
Shaheen <shn.sdq29@gmail.com>
2024-11-16 02:15:14 +0530

    commit

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,9 @@
 import { Router } from "express";
 import {registerUser} from "../controllers/user.controller.js";
+import {loginUser} from "../controllers/user.controller.js";
+import {logoutUser} from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.middleware.js"
+import {verifyJWT} from "../middlewares/auth.middleware.js";
 
 const router= Router()
 
@@ -17,5 +20,10 @@ router.route("/register").post(
 
     ]),
     registerUser)
+
+router.route("/login").post(loginUser)
+
+//secured routes
+router.route("/logout").post(verifyJWT, logoutUser)
 
 export default router
