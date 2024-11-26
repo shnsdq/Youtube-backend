f7ee7fd63caacc6aa526d0f696aa749c65fe692c
Shaheen <shn.sdq29@gmail.com>
2024-11-26 18:24:05 +0530

    commit changes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -29,7 +29,7 @@ router.route("/current-user").post(verifyJWT, getCurrentUser)
 router.route("/update-account").patch(verifyJWT, updateAccountDetails)
 
 router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
-router.route("/cover-image").patch(verifyJWT, upload.single("/coverImage"), updateUserCoverImage)
+router.route("/cover-image").patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage)
 
 router.route("/c/:username").get(verifyJWT, getUserChannelProfile)
 router.route("/history").get(verifyJWT, getWatchHistory)
